1189a7c762a2117a7f94e3dfddeef30d2e0d4d6a
matheusluna <matheusluna96@gmail.com>
2018-04-16 06:02:31 -0300

    adicionando outras funcionalidades

---
+++ src/main/java/controllers/CadastroAtendenteServico.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import entidades.Atendente;
+import entidades.AtendenteServico;
+import entidades.Servico;
+import fabricas.DaoFabricaBDPostgres;
+import interfaces.Comando;
+import interfaces.DaoAtendenteInterface;
+import interfaces.DaoAtendenteServicoInterface;
+import interfaces.DaoServicoInterface;
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author mathe
+ */
+public class CadastroAtendenteServico implements Comando{
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws SQLException, ClassNotFoundException, IOException, ServletException {
+        DaoFabricaBDPostgres daoFabricaBDPostgres = new DaoFabricaBDPostgres();
+        DaoAtendenteInterface daoAtendente = daoFabricaBDPostgres.criarAtendenteDao();
+        DaoServicoInterface daoServico = daoFabricaBDPostgres.criarServicoDao();
+        DaoAtendenteServicoInterface daoAtendenteServico = daoFabricaBDPostgres.criarAtendenteServicoDao();
+        
+        Atendente atendente = daoAtendente.read(request.getParameter("email"));
+        Servico servico = daoServico.read(Integer.parseInt(request.getParameter("servico")), "");
+        int tempo = Integer.valueOf(request.getParameter("tempo"));
+        
+        AtendenteServico atendenteServico = new AtendenteServico(atendente, servico, tempo);
+        
+        if(daoAtendenteServico.create(atendenteServico)){
+            request.getRequestDispatcher("principalAdmin.jsp").forward(request, response);
+        }else{
+            request.getRequestDispatcher("cadastroAtendenteServico.jsp").forward(request, response);
+        }
+        
+    }
+    
+}
---
+++ src/main/webapp/escolhaAtendimento.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : escolhaAtendimento
+    Created on : 16/04/2018, 05:46:41
+    Author     : mathe
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <ul>
+            <li><a href="Frontal?caminho=Atendentes">Atendentes</a></li>
+            <li><a href="Frontal?caminho=Sevicos">Serviços</a></li>
+        </ul>
+    </body>
+</html>
