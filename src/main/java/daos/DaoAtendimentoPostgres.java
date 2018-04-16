26315c24309b594279d5af990c93ec5f4a6a1e24
matheusluna <matheusluna96@gmail.com>
2018-04-15 21:21:23 -0300

    adicionando dao de atendentes

--- src/main/java/daos/DaoAtendimentoPostgres.java
+++ src/main/java/daos/DaoAtendimentoPostgres.java
@@ -6,10 +6,17 @@
 package daos;
 
 import entidades.Atendimento;
+import fabricas.ConnectionFactory;
 import interfaces.DaoAtendimentoInterface;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -24,7 +31,19 @@ public class DaoAtendimentoPostgres implements DaoAtendimentoInterface{
 
     @Override
     public Atendimento read(String cliente, String atendente, LocalDate dia, LocalDateTime hora) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Atendimento atendimento = null;
+        try {
+            Connection connection = new ConnectionFactory().getConnection();
+            String sql = "select * from atendimento where cliente = ?, atendente = ?, dia = ?, inicio = ?";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, cliente);
+            stmt.setString(2, atendente);
+            stmt.setDate(3, date);
+            stmt.setTime(4, time);
+            ResultSet rs = stmt.executeQuery();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DaoAtendimentoPostgres.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
